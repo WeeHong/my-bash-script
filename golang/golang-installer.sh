9e38e92f389d32423a222a1688cfe30cbee24799
Wee Hong <weehongayden.developer@hotmail.com>
2022-02-03 22:20:36 +0800

    Update golang-installer.sh

--- golang/golang-installer.sh
+++ golang/golang-installer.sh
@@ -15,6 +15,6 @@ GOLANG_VERSION=$(echo "$RESPONSE_BODY" | sed -E 's/[0-9]{3}$//')
 # Add Golang to the Path environment variable
 export PATH=$PATH:/usr/local/go/bin
 
-source $HOME/.zshrc
+. $HOME/.zshrc
 
 # export GOPATH="/mnt/c/Users/${user}/directory/to/your/golang/workspace"
