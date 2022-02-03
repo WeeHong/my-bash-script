d518c5bccd2288e4fe029e6fe9c884568c8e1ab4
Wee Hong <weehongayden.developer@hotmail.com>
2022-02-03 14:08:24 +0800

    Update golang-installer.sh

--- golang-installer.sh
+++ golang-installer.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-. script
+cd ~ || exit
 
 # Extract the latest Golang version
-RESPONSE_BODY=$(curl -s -w "\n%{http_code}" https://go.dev/VERSION\?m\=text)
-GOLANG_VERSION=$(echo $RESPONSE_BODY | sed -E 's/[0-9]{3}$//')
+RESPONSE_BODY=$(curl -s -w "\n%{http_code}" https://go.dev/VERSION\?m=text)
+GOLANG_VERSION=$(echo "$RESPONSE_BODY" | sed -E 's/[0-9]{3}$//')
 
 # Download the latest Golang tape archive for Linux
 curl -O "https://go.dev/dl/$GOLANG_VERSION.linux-amd64.tar.gz"
 
 # Change the directory and extract the compress the files
-tar -C /usr/local -xzf $GOLANG_VERSION.linux-amd64.tar.gz
+tar -C /usr/local -xzf "$GOLANG_VERSION".linux-amd64.tar.gz
 
 # Add Golang to the Path environment variable
 export PATH=$PATH:/usr/local/go/bin
