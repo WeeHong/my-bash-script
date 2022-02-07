a15fc9d1bed20d0ae1b912dcdf3bd5c805f7e0a4
Wee Hong <weehongayden.developer@hotmail.com>
2022-02-07 23:13:53 +0800

    Update installer.sh

--- python/installer.sh
+++ python/installer.sh
@@ -1,40 +1,5 @@
 #!/bin/bash
 
-# Define Operating System and Architecture
-OS="$(uname -s)"
-ARCH="$(uname -m)"
-
-case $OS in
-    "Linux")
-        case $ARCH in
-        "x86_64")
-            ARCH=amd64
-            ;;
-        "aarch64")
-            ARCH=arm64
-            ;;
-        "armv6" | "armv7l")
-            ARCH=armv6l
-            ;;
-        "armv8")
-            ARCH=arm64
-            ;;
-        .*386.*)
-            ARCH=386
-            ;;
-        esac
-        PLATFORM="linux-$ARCH"
-    ;;
-    "Darwin")
-        PLATFORM="darwin-amd64"
-    ;;
-esac
-
-if [ -z "$PLATFORM" ]; then
-    echo "Your operating system is not supported by the script."
-    exit 1
-fi
-
 # Checking Shell
 if [ -n "echo $ZSH_VERSION" ]; then
     shell_profile="$HOME/.zshrc"
@@ -60,26 +25,20 @@ PYTHON_VERSION=$(echo "$RESPONSE_BODY" | sed -E 's/[0-9]{3}$//')
 # Download Python3
 echo "Download Python3"
 (cd ~ && curl -OL "https://www.python.org/ftp/python/$PYTHON_VERSION/Python-$PYTHON_VERSION.tgz")
-
-# Define GOROOT AND GOPATH path
-FOLDER_VERSION=${PYTHON_VERSION%.*}
-[ -z "$PYTHON_ROOT" ] && PYTHON_ROOT="/usr/bin/python$FOLDER_VERSION"
+EXTRACT_VERSION=${PYTHON_VERSION%.*}
 
 # Installing Python3
 echo "Installing Python3"
 (cd ~ \
-  && mkdir -p $PYTHON_ROOT \
-  && tar -C $PYTHON_ROOT -xvf Python-$PYTHON_VERSION.tgz \
-  && cd $PYTHON_ROOT/Python-$PYTHON_VERSION \
+  && tar -xvf Python-$PYTHON_VERSION.tgz \
+  && cd Python-$PYTHON_VERSION \
   && ./configure --with-ensurepip=install \
   && make -j8 \
   && sudo make install \
-  && alias python3="python$FOLDER_VERSION" \
-  && alias python="python$FOLDER_VERSION" \
+  && alias python3="python$EXTRACT_VERSION" \
+  && alias python="python$EXTRACT_VERSION" \
   && cd ~ \
-  && rm -rf Python-$PYTHON_VERSION.tgz PYTHON_ROOT)
-  
-alias python="python$FOLDER_VERSION"
+  && rm -rf Python-$PYTHON_VERSION.tgz Python-$PYTHON_VERSION)
 
 # Checking Shell
 echo "Checking Shell"
@@ -95,23 +54,6 @@ elif [ -n "echo $FISH_VERSION" ]; then
         shell_profile="$HOME/.config/fish/config.fish"
     fi
 fi
-
-# echo "Configuring shell profile in: $shell_profile"
-# touch "$shell_profile"
-# if [ "$shell" == "fish" ]; then
-#     {
-#         echo -e '\n'
-#         echo '# Python'
-#         echo "set PYTHON_ROOT '${PYTHON_ROOT}'"
-#     } >> "$shell_profile"
-# else
-#     {
-#         echo -e '\n'
-#         echo '# Python'
-#         echo "export PYTHON_ROOT=${PYTHON_ROOT}"
-#         echo 'export PATH=$PYTHON_ROOT/bin:$PATH'
-#     } >> "$shell_profile"
-# fi
 
 # Install Python3 PIP
 echo "Installing Python3 PIP"
