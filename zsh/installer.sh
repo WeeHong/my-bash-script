74ed5c97fd5d1f750c5b4f34f76aee3a77e04523
Wee Hong <weehongayden.developer@hotmail.com>
2022-02-12 14:23:57 +0800

    Update installer.sh

--- zsh/installer.sh
+++ zsh/installer.sh
@@ -8,6 +8,8 @@ sudo apt-get install git -y
 git config --global user.email "weehongayden.developer@hotmail.com"
 git config --global user.name "Wee Hong KOH"
 
+sudo apt-get -y upgrade && sudo apt-get -y update
+
 sudo apt-get install zsh -y
 echo "# Install Oh My Zsh"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
