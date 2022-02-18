20cbd898ca12784e093871ae2fd78648d3bc9ebf
Wee Hong <weehongayden.developer@hotmail.com>
2022-02-18 11:51:47 +0800

    Update installer.sh

--- docker/installer.sh
+++ docker/installer.sh
@@ -19,7 +19,7 @@ sudo apt-get install \
 curl -fsSL https://download.docker.com/linux/${DISTRO_ID}/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
 echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/${DISTRO_ID} \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 sudo apt-get update
